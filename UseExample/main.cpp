a68a126ab29cf0f7e3d7e4c8057aadc228032aff
Elad Eliav <elad02.eliav@gmail.com>
2019-03-15 00:51:04 +0200

    IT WORKS made usage example

---
+++ UseExample/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "UniSocket.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    try
+    {
+        UniSocket uniSock(5400, SOMAXCONN);
+    }catch(UniSocketException& e)
+    {
+        std::cout << e._errorMsg << std::endl;
+    }
+
+
+    return 0;
+}
