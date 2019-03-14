3db6b40b59dd0f93484f003766a4e2745cca7826
Elad Eliav <elad02.eliav@gmail.com>
2019-03-14 10:42:42 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <boost/algorithm/string.hpp>
+
+using json = nlohmann::json;
+
+using namespace std;
