feb4b6c249ce28dcf9edc009378d21b14f496e4c
Elad Eliav <elad02.eliav@gmail.com>
2019-03-15 15:01:02 +0200

    made examples

---
+++ EchoServerExample/Client/Client.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Elad Eliav on 2019-03-15.
+//
+#include <iostream>
+#include "UniSocket.h"
+
+using std::string;
+
+int main()
+{
+    try
+    {
+        UniSocket client("127.0.0.1", 2600);
+        string sendString;
+
+        do
+        {
+            std::cout << ">";
+            std::cin >> sendString;
+            client.send(sendString);
+            std::cout << "The Server sent back: " << client.recv() << std::endl;
+        }while(sendString != "EXIT");
+        client.close();
+    }catch(UniSocketException e)
+    {
+        std::cout << e << std::endl;
+    }
+
+
+    return 0;
+}
+
